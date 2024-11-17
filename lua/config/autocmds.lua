4142303e72975d05c97138763c8fe225440a470d
Ran Shaham <ransh@mobileye.com>
2024-11-17 09:59:26 +0200

    make inlay hints the same color as comments

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,3 +1,6 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- Set LSP inlay hints to use the same color as comments
+vim.api.nvim_set_hl(0, "LspInlayHint", { link = "Comment" })
